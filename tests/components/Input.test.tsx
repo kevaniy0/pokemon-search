cde4e932a4f8208553dcff66fb406eb8d55461bd
Vsevolod Goriachev <kevlarenff@gmail.com>
2025-09-14 04:21:47 +0300

    test: add basic test for Input component

---
+++ tests/components/Input.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Input from 'src/components/Input';
+describe('Input', () => {
+  it('should render input component', () => {
+    render(<Input />);
+    const input = screen.getByPlaceholderText(/type a pokemon name/i);
+    expect(input).toBeInTheDocument();
+    screen.debug();
+  });
+});
