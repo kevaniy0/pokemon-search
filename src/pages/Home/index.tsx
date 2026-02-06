6417d3ac0b58e95b0ea76aa399e836869d5894d5
Vsevolod Goriachev <kevlarenff@gmail.com>
2026-02-06 19:33:26 +0300

    feat: add custom hook for localStorage actions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,59 +4,33 @@ import TopControls from '@/views/TopControls';
 import type { HomePageState } from '@/types/props';
 import errorImg from 'assets/error2.jpg';
 import { useEffect, useState } from 'react';
-import type { Pokemon } from '@/types/pokemon';
 import getPokemon from '@/services/PokemonAPI';
 import extractData from '@/utils/extractData';
-import { validatePokemonData } from '@/utils/validateData';
 import getDelay from '@/utils/getDelay';
 import HttpError from '@/services/HttpError';
 import { Navigate, Outlet, useNavigate, useParams } from 'react-router';
 import Pagination from '@/components/Pagination';
+import { usePokemonStorage } from '@/hooks/usePokemonStorage';
 
 const baseState: HomePageState = {
   inputValue: '',
-  searchItem: '',
-  results: [],
   error: null,
   isLoading: false,
-  forceError: false,
 };
 
 const HomePage = () => {
-  const [state, setState] = useState(baseState);
-  const [isInitialized, setIsInitialized] = useState(false);
-  const { page } = useParams();
-  const navigate = useNavigate();
+  const storage = usePokemonStorage();
+  const [state, setState] = useState<HomePageState>(baseState);
 
   useEffect(() => {
-    const history = localStorage.getItem('searchHistory');
-    const searchItem = localStorage.getItem('searchItem') || '';
-
-    if (!history) {
-      setIsInitialized(true);
-      return;
+    if (storage.isInitialized) {
+      setState((prev) => ({ ...prev, inputValue: storage.searchItem }));
     }
-    try {
-      const parsedData: unknown = JSON.parse(history);
-      if (validatePokemonData(parsedData)) {
-        setState((prev) => ({ ...prev, results: parsedData, searchItem }));
-      } else {
-        throw new Error('Invalid Pokemon data format');
-      }
-    } catch (error) {
-      localStorage.removeItem('searchHistory');
-      localStorage.removeItem('searchItem');
-      if (error instanceof Error) {
-        console.error('Failed to parse search history:', error.message);
-      } else {
-        console.error('unknown localstorage error:', error);
-      }
-    } finally {
-      setIsInitialized(true);
-    }
-  }, []);
+  }, [storage.isInitialized, storage.searchItem]);
 
-  if (!isInitialized) return null;
+  const { page } = useParams();
+  const navigate = useNavigate();
+  if (!storage.isInitialized) return null;
 
   const correctPage = Number(page);
   if (!correctPage || isNaN(correctPage) || correctPage < 1) {
@@ -66,57 +40,44 @@ const HomePage = () => {
   const itemsPerPage = 6;
   const startIndex = (correctPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentItems = state.results.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(state.results.length / itemsPerPage);
+  const currentItems = storage.results.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(storage.results.length / itemsPerPage);
 
-  if (isInitialized && state.results.length === 0 && correctPage !== 1) {
+  if (storage.results.length === 0 && correctPage !== 1) {
     return <Navigate to="/home/1" replace />;
   }
-  if (isInitialized && state.results.length > 0 && correctPage > totalPages) {
+  if (storage.results.length > 0 && correctPage > totalPages) {
     return <Navigate to={`/home/${totalPages}`} replace />;
   }
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState((prev) => ({ ...prev, searchItem: e.target.value }));
-  };
-
-  const clearForceError = () => {
-    setState((prev) => ({ ...prev, forceError: false }));
+    setState((prev) => ({ ...prev, inputValue: e.target.value }));
   };
 
   const handlePageChange = (page: number) => {
-    console.log('change page');
-
-    localStorage.setItem('pokemonLastPage', String(page));
+    storage.updateLastPage(page);
     navigate(`/home/${page}`);
   };
   const handleSearch = async () => {
-    clearForceError();
-    setState((prev) => ({ ...prev, inputValue: prev.searchItem }));
-    const value = state.searchItem.trim();
+    const value = state.inputValue.trim();
     if (value === '') return;
 
-    localStorage.setItem('searchItem', value);
-
+    storage.updateSearchValue(value);
     setState((prev) => ({ ...prev, isLoading: true }));
-
-    const history: Pokemon[] = JSON.parse(
-      localStorage.getItem('searchHistory') || '[]'
-    );
 
     const startLoadingTime = Date.now();
     try {
       const pokemon = await getPokemon(value);
       const data = extractData(pokemon);
-      const checkedHistory = history.filter((item) => item.name !== data.name);
+      const checkedHistory = storage.results.filter(
+        (item) => item.name !== data.name
+      );
       const newHistory = [data, ...checkedHistory];
       const delay = getDelay(startLoadingTime);
-      localStorage.setItem('searchHistory', JSON.stringify(newHistory));
       setTimeout(() => {
+        storage.updateResults(newHistory);
         setState((prev) => ({
           ...prev,
-          searchItem: value,
-          results: newHistory,
           isLoading: false,
           error: null,
         }));
@@ -128,7 +89,7 @@ const HomePage = () => {
         setTimeout(() => {
           setState((prev) => ({
             ...prev,
-            searchItem: value,
+            inputValue: value,
             error: {
               status: error.status,
               message: error.message,
@@ -156,7 +117,7 @@ const HomePage = () => {
     <div className="flex flex-col gap-y-4 h-[100%]">
       <TopControls
         isLoading={state.isLoading}
-        value={state.searchItem}
+        value={state.inputValue}
         onChange={handleInput}
         onClick={handleSearch}
         error={state.error}
@@ -182,7 +143,7 @@ const HomePage = () => {
       >
         <section className="results-section flex items-center gap-y-2.5 gap-x-4 justify-center px-4">
           <Results results={currentItems} isLoading={state.isLoading} />
-          <Outlet context={{ results: state.results }} />
+          <Outlet context={{ results: storage.results }} />
         </section>
         <Pagination
           current={Number(page)}
