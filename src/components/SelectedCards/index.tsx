6e9103ea863446419c76964c058f8787c9655788
Vsevolod Goriachev <kevlarenff@gmail.com>
2026-02-21 16:36:15 +0300

    feat: add opportunity to download selected items info

--- src/components/SelectedCards/index.tsx
+++ src/components/SelectedCards/index.tsx
@@ -3,11 +3,42 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import Button from '../Button';
 import { btnClasses } from '../Pagination/pagination-classes';
 import { resetItems } from '@/store/cards/cardsSlice';
+import { validatePokemonData } from '@/utils/validateData';
 
 export const SelectedCards = () => {
   const dispatch = useAppDispatch();
   const cards = useAppSelector(selectorAll);
+  const handleDownloadItems = () => {
+    const elements = localStorage.getItem('searchHistory');
+    if (!elements) return;
+    const parsedData = JSON.parse(elements);
+    if (!validatePokemonData(parsedData)) throw new Error('Error Parse Data');
+    const extractedPokemons = parsedData.filter((item) =>
+      cards.includes(item.name)
+    );
+    const headers = ['Name', 'Type', 'Height', 'Weight', 'Img'];
 
+    const rows = extractedPokemons.map((pokemon) => [
+      pokemon.name,
+      pokemon.types.map((item) => item.type.name).join(', ') || 'unknown',
+      pokemon.height || '',
+      pokemon.weight || '',
+      pokemon.pic,
+    ]);
+
+    const csvContent = [
+      headers.join(','),
+      ...rows.map((row) => row.join(',')),
+    ].join('\n');
+
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${extractedPokemons.length}selectedCards.csv`;
+    link.click();
+    URL.revokeObjectURL(url);
+  };
   if (cards.length === 0) return null;
   return (
     <div className="sticky bottom-2 mb-2  selected-cards w-max p-7 flex flex-col items-center gap-y-4  border-2 border-xDark dark:border-xLight rounded-2xl bg-xLight dark:bg-xDark">
@@ -21,7 +52,11 @@ export const SelectedCards = () => {
           className={btnClasses}
           onClick={() => dispatch(resetItems())}
         />
-        <Button name="Download" className={btnClasses} />
+        <Button
+          name="Download"
+          className={btnClasses}
+          onClick={handleDownloadItems}
+        />
       </div>
     </div>
   );
