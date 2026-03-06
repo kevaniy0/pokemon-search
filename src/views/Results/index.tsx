fe14b594f01695a872dea70c3f1a38e68f697156
Vsevolod Goriachev <kevlarenff@gmail.com>
2026-03-06 22:36:16 +0300

    feat: add tests for PokemonApi

--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -3,10 +3,6 @@ import CardList from '../../components/CardList';
 import type { ResultProps } from '../../types/props';
 
 const Results = (props: ResultProps) => {
-  if (props.forceError) {
-    throw new Error('Искусственная ошибка');
-  }
-
   return (
     <div className="card-list-wrapper">
       <CardList isLoading={props.isLoading} results={props.results} />
