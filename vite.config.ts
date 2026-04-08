be46174aaf19a8da9303ac04a31fba2a6b39391a
Vsevolod Goriachev <kevlarenff@gmail.com>
2026-04-08 14:46:09 +0300

    fix: resolve deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'url';
 import removeAttr from 'react-remove-attr';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
-  const plugins = [react(), tailwindcss(), svgr(), tsconfigPaths()];
+  const plugins = [react(), tailwindcss(), svgr()];
   if (isProduction) {
     plugins.unshift(
       removeAttr({
@@ -25,6 +27,7 @@ export default defineConfig(({ mode }) => {
         assets: path.resolve(__dirname, './src/assets'),
         components: path.resolve(__dirname, './src/components'),
       },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     },
     server: {
       open: true,
