3fdc4cee98fec3495a1459aa6624eb14b711fca4
Vsevolod Goriachev <kevlarenff@gmail.com>
2026-04-17 03:21:15 +0300

    docs: update README and about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,9 +3,8 @@ const AboutPage = () => {
     <section className="flex flex-col justify-between max-w-4xl mx-auto px-6 text-xDark dark:text-xLight h-[100%] font-[400] mb-5">
       <p className="text-lg leading-relaxed mb-6">
         This app allows you to search for Pokémon by name, view their images and
-        stats, and manage your search history. It uses the{' '}
-        <span className="font-semibold">PokéAPI</span> - a free public API that
-        provides detailed Pokémon data.
+        stats. It uses the <span className="font-semibold">PokéAPI</span> - a
+        free public API that provides detailed Pokémon data.
       </p>
       <h2 className="text-[20px] font-bold text-xDark dark:text-xLight mb-1">
         Stack:
@@ -42,10 +41,6 @@ const AboutPage = () => {
           Vercel <span className="font-normal">(deployment)</span>
         </li>
         <li>
-          localStorage{' '}
-          <span className="font-normal">(saving search history)</span>
-        </li>
-        <li>
           Eslint, Prettier, Husky{' '}
           <span className="font-normal">(code quality and formatting)</span>
         </li>
@@ -60,19 +55,15 @@ const AboutPage = () => {
       <ul className="list-disc list-inside space-y-3 text-lg">
         <li>
           Type a Pokémon name into the search bar and click{' '}
-          <span className="font-semibold">Search</span>. (
-          <span className="underline underline-offset-2">
-            {"You can use numbers from 1 to 1025 if you don't know the names"}
-          </span>
-          )
+          <span className="font-semibold">Search</span>.
         </li>
         <li>
           If the Pokémon exists, its card with image and details will appear.
         </li>
+        <li>Click on the card to open the description.</li>
         <li>
-          Your previous searches are saved in{' '}
-          <span className="font-semibold">localStorage</span> so you can revisit
-          them anytime.
+          Select pokemons to download additional information about them in a CSV
+          file.
         </li>
       </ul>
     </section>
